1d0e9efd893d390aeff96a79ec7f38e45b01270c
benevolentimp <jaakkola.jooa@gmail.com>
2022-08-31 10:47:30 +0300

    Hello World -program

---
+++ HelloWorld/HelloWorld.c
@@ -0,0 +1,14 @@
+/*
+ * Title        : HelloWorld.c
+ * Author       : @benevolentimp
+ * Description  : Every programmers first word.
+ */
+
+#include <stdio.h>
+
+int main() 
+{
+  printf("Hello, World!\n");
+
+  return 0;
+}
